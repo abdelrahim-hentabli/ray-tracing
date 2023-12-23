3605e6d7761915588461ffa401b9c090e01c8084
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2023-12-23 15:20:06 -0800

    initial commit

---
+++ build/CMakeFiles/ray_tracer.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray_tracer.dir/box.cpp.o"
+  "CMakeFiles/ray_tracer.dir/box.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/camera.cpp.o"
+  "CMakeFiles/ray_tracer.dir/camera.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/dump_png.cpp.o"
+  "CMakeFiles/ray_tracer.dir/dump_png.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/flat_shader.cpp.o"
+  "CMakeFiles/ray_tracer.dir/flat_shader.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/hierarchy.cpp.o"
+  "CMakeFiles/ray_tracer.dir/hierarchy.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/main.cpp.o"
+  "CMakeFiles/ray_tracer.dir/main.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/mesh.cpp.o"
+  "CMakeFiles/ray_tracer.dir/mesh.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/parse.cpp.o"
+  "CMakeFiles/ray_tracer.dir/parse.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/phong_shader.cpp.o"
+  "CMakeFiles/ray_tracer.dir/phong_shader.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/plane.cpp.o"
+  "CMakeFiles/ray_tracer.dir/plane.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/reflective_shader.cpp.o"
+  "CMakeFiles/ray_tracer.dir/reflective_shader.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/render_world.cpp.o"
+  "CMakeFiles/ray_tracer.dir/render_world.cpp.o.d"
+  "CMakeFiles/ray_tracer.dir/sphere.cpp.o"
+  "CMakeFiles/ray_tracer.dir/sphere.cpp.o.d"
+  "ray_tracer"
+  "ray_tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray_tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ray_tracer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ray_tracer.
+# This may be replaced when dependencies are built.
