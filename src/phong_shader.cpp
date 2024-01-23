945c5b8695721c9ca29e6b9d4ff502022e258e39
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2024-01-23 08:37:04 -0800

    specular working

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -13,16 +13,24 @@ Shade_Surface(const Ray& ray, const vec3& intersection_point,
 
     vec3 currentLightIntensity;
     vec3 currentLightDir;
+    vec3 reflectionDir;
+    vec3 normalizedLightDir;
+
     for (Light *light: world.lights){
         currentLightDir = light->position - intersection_point;
         if (dot(currentLightDir, normal) < 1e-6) {
             continue;
         }
         else {
+            normalizedLightDir = currentLightDir.normalized();
             currentLightIntensity = light->Emitted_Light(currentLightDir);
-            diffuse += currentLightIntensity * std::max(0.0, dot(normal,currentLightDir.normalized()));
+            diffuse += currentLightIntensity * std::max(0.0, dot(normal,normalizedLightDir));
+
+            reflectionDir = 2 * dot(normalizedLightDir, normal) * normal - normalizedLightDir;
+            specular += currentLightIntensity * std::pow(std::max(0.0,dot(reflectionDir, -ray.direction)),specular_power);
+             
         }
     }    
-    vec3 color = specular + world.ambient_intensity * color_ambient * world.ambient_color + diffuse * color_diffuse;
+    vec3 color = specular * color_specular + world.ambient_intensity * color_ambient * world.ambient_color + diffuse * color_diffuse;
     return color;
 }
