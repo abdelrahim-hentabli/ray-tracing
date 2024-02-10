4bb6182a294e5bf8e531a1714a61cd3d95e5ad1c
Rahim Hentabli <abdelrahim.hentabli@gmail.com>
2024-02-09 20:50:11 -0800

    basic hierarchy working (#19)

--- src/box.cpp
+++ src/box.cpp
@@ -4,7 +4,37 @@
 // Return whether the ray intersects this box.
 bool Box::Intersection(const Ray& ray) const
 {
-    TODO;
+    double tmin = (lo[0] - ray.endpoint[0]) / ray.direction[0];
+    double tmax = (hi[0] - ray.endpoint[0]) / ray.direction[0];
+    
+    if (tmin > tmax) {
+        std::swap(tmin, tmax); 
+    }
+
+    double tymin = (lo[1] - ray.endpoint[1]) / ray.direction[1];
+    double tymax = (hi[1] - ray.endpoint[1]) / ray.direction[1];
+    
+    if (tymin > tymax) {
+        std::swap(tymin, tymax); 
+    }
+    if ((tmin > tymax) || (tymin > tmax)) {
+        return false; 
+    }
+    
+    tmin = std::max(tmin,tymin);
+    tmax = std::min(tmax,tymax);
+    
+    double tzmin = (lo[2] - ray.endpoint[2]) / ray.direction[2];
+    double tzmax = (hi[2] - ray.endpoint[2]) / ray.direction[2];
+
+    if (tzmin > tzmax) {
+        std::swap(tzmin, tzmax);
+    }
+
+    if ((tmin > tzmax) || (tzmin > tmax)) {
+        return false; 
+    }
+    
     return true;
 }
 
@@ -12,14 +42,20 @@ bool Box::Intersection(const Ray& ray) const
 Box Box::Union(const Box& bb) const
 {
     Box box;
-    TODO;
+    for(int axis = 0; axis < 3; axis++){
+        box.hi[axis] = std::max(hi[axis], bb.hi[axis]);
+        box.lo[axis] = std::min(lo[axis], bb.lo[axis]);
+    }
     return box;
 }
 
 // Enlarge this box (if necessary) so that pt also lies inside it.
 void Box::Include_Point(const vec3& pt)
 {
-    TODO;
+    for(int axis = 0; axis < 3; axis++){
+        hi[axis] = std::max(hi[axis], pt[axis]);
+        lo[axis] = std::min(lo[axis], pt[axis]);
+    }
 }
 
 // Create a box to which points can be correctly added using Include_Point.
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -44,7 +44,39 @@ void Mesh::Read_Obj(const char* file)
 // Check for an intersection against the ray.  See the base class for details.
 Hit Mesh::Intersection(const Ray& ray, int part) const
 {
-    TODO;
+    double dist;
+    if(part >= 0){
+        if(part >= number_parts){
+            return {};
+        }
+        if(!Bounding_Box(part).Intersection(ray)){
+            return {};
+        }
+        if(Intersect_Triangle(ray,part,dist)){
+            return {this, dist, part};
+        }
+    }
+    else{
+        if(!box.Intersection(ray)){
+            return {};
+        }
+        int min_part = -1;
+        double min_dist = std::numeric_limits<double>::infinity();
+        for(int i = 0; i < number_parts; i++){
+            if(Intersect_Triangle(ray,i,dist)){
+                if(dist < min_dist){
+                    min_dist = dist;
+                    min_part = i;
+                }
+            }
+        }
+        if(min_part == -1){
+            return {};
+        }
+        else{
+            return {this,min_dist,min_part};
+        }
+    }
     return {};
 }
 
@@ -52,8 +84,12 @@ Hit Mesh::Intersection(const Ray& ray, int part) const
 vec3 Mesh::Normal(const vec3& point, int part) const
 {
     assert(part>=0);
-    TODO;
-    return vec3();
+    vec3 point0 = vertices[triangles[part][0]];
+    vec3 point1 = vertices[triangles[part][1]];
+    vec3 point2 = vertices[triangles[part][2]];
+
+    vec3 normal =  cross(point1 - point0, point2 - point0).normalized();
+    return normal;
 }
 
 // This is a helper routine whose purpose is to simplify the implementation
@@ -70,8 +106,45 @@ vec3 Mesh::Normal(const vec3& point, int part) const
 // two triangles.
 bool Mesh::Intersect_Triangle(const Ray& ray, int tri, double& dist) const
 {
-    TODO;
-    return false;
+    ivec3 triangle = triangles[tri];
+    vec3 point0 = vertices[triangle[0]];
+    vec3 point1 = vertices[triangle[1]];
+    vec3 point2 = vertices[triangle[2]];
+    
+    vec3 normal =  cross(point1 - point0, point2 - point0);
+
+    double bespoke_weight_tolerance = normal.magnitude_squared() *  - weight_tol;
+
+    if(abs(dot(normal,ray.direction)) < -1e-4) {
+        return false;
+    }
+
+    double d = -dot(normal, point0);
+
+    double t = -(dot(normal, ray.endpoint) + d) / dot(normal,ray.direction);
+    if (t < small_t){
+        return false;
+    }
+
+    vec3 p = ray.endpoint + t * ray.direction;
+
+    vec3 c = cross(point1 - point0, p - point0);
+    if (dot(normal,c) < bespoke_weight_tolerance) {
+        return false;
+    }
+
+    c = cross(point2 - point1, p - point1);
+    if (dot(normal, c) < bespoke_weight_tolerance){
+        return false;
+    }
+    c = cross(point0 - point2, p - point2);
+    if (dot(normal, c) < bespoke_weight_tolerance) {
+        return false;
+    }
+
+    dist = t;
+    
+    return true;
 }
 
 // Compute the bounding box.  Return the bounding box of only the triangle whose
@@ -79,6 +152,9 @@ bool Mesh::Intersect_Triangle(const Ray& ray, int tri, double& dist) const
 Box Mesh::Bounding_Box(int part) const
 {
     Box b;
-    TODO;
+    b.Make_Empty();
+    b.Include_Point(vertices[triangles[part][0]]);
+    b.Include_Point(vertices[triangles[part][1]]);
+    b.Include_Point(vertices[triangles[part][2]]);
     return b;
 }
