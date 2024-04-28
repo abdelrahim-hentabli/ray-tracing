78480d33c705059dd073877594dd4d626d811864
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2024-04-28 16:49:37 -0700

    using macro for av_err2str

--- tools/common/dump_mp4.cpp
+++ tools/common/dump_mp4.cpp
@@ -36,6 +36,9 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
+#undef av_err2str
+#define av_err2str(errnum) av_make_error_string((char*)__builtin_alloca(AV_ERROR_MAX_STRING_SIZE), AV_ERROR_MAX_STRING_SIZE, errnum)
+
 #define STREAM_DURATION 10.0
 #define STREAM_FRAME_RATE 25 /* 25 images/s */
 
